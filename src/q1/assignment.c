0dda37b12d777d96a9f366c32d18bd964d803855
PETER CONWAY <P.CONWAY9@nuigalway.ie>
2018-10-25 12:55:38 +0100

    Added sizeof lines

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
+	int  x;
+	int *y;
+	long z;
+	double *a;
+	char **b;
+	printf("%d\n", sizeof(x));
+	printf("%d\n", sizeof(y));
+	printf("%d\n", sizeof(z));
+	printf("%d\n", sizeof(a));
+	printf("%d\n", sizeof(b));
   return 0;
 }
